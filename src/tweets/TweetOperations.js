934a600225f2352b72f862bc9df4248434f5b285
hyobinnSeo <103991723+hyobinnSeo@users.noreply.github.com>
2024-12-03 19:07:37 +0900

    태그 사용 방지 프롬프트

--- src/tweets/TweetOperations.js
+++ src/tweets/TweetOperations.js
@@ -93,7 +93,7 @@ class TweetOperations {
             }
 
             const systemPrompt = this.personality.prompt + '\n' + this.personality.name + '\n' + this.personality.title + '\n' + this.personality.years + '\n' + this.personality.characteristics + '\n' + this.personality.trivia + '\n' + this.personality.guidelines + '\n' + tweetContext + 
-                "\nIMPORTANT: Don't use hashtags in your tweet. Simply write the tweet content." + "\nIMPORTANT: Your response MUST be under 280 characters. If you exceed this limit, your tweet will be truncated.";
+                "\nIMPORTANT: Don't use @ to tag anyone, and no hashtags in your tweet. Simply write the tweet content." + "\nIMPORTANT: Your response MUST be under 280 characters. If you exceed this limit, your tweet will be truncated.";
             const userPrompt = "Generate a single tweet (max 280 characters) reacting to the recent tweets while maintaining your historical persona. Be concise and impactful.";
 
             console.log('\nComplete prompt being sent to OpenRouter:');
